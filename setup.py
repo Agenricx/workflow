8844b4b0f5c3fc7e40d1ec201dbad67b75f55dbe
wangzepeng03 <wangzepeng03@meituan.com>
2024-06-03 21:27:28 +0800

    update: make setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+"""Alfred-Workflow library with Python for building Alfred 3.x 4.x 5.x workflows."""
+
+from distutils.core import setup
+
+setup(
+    name='workflow',
+    version='1.40.0',
+    description='Alfred-Workflow library with Python for building Alfred 3.x 4.x 5.x workflows.',
+    author='Agenric',
+    author_email='AgenricWon@gmail.com',
+    url='https://github.com/agenric/Workflow',
+    packages = ['workflow'],
+    include_package_data=True,
+    package_data = {'workflow': ['version', 'Notify.tgz']}
+)
